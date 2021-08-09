cfd6b16093f92332d03fe3ee9f90a3fc58f610b6
ah00ee <nhoahyoung@gmail.com>
2021-08-10 01:05:48 +0900

    commit

--- bot.py
+++ bot.py
@@ -1,25 +1,50 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 import datetime
 import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-def new_cases():
-    driver = webdriver.Chrome(executable_path='C:\\Users\\nhoah\Downloads\chromedriver_win32/chromedriver.exe')
-    url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%ED%99%95%EC%A7%84%EC%9E%90+%EC%88%98'
-    driver.get(url)
-    time.sleep(2)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    t = soup.select('#_cs_production_type > div > div:nth-child(4) > div > div:nth-child(5) > div._normality > div.confirmed_status.new > div > div.tooltip_area._tooltip_wrapper > dl > div:nth-child(1) > dd.desc_em._total')[0].text
-    return t
+def crawl_cases():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(executable_path='C:\\Users\\nhoah\Downloads\chromedriver_win32/chromedriver.exe', options=options)
 
+    url = 'https://corona-live.com/'
+    driver.get(url)
+    time.sleep(1)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    today = soup.select('#__next > div.MainLayout__Wrapper-fslwZe.dhgRDa > div.MainLayout__Main-eibkLu.bNtKwG > div.Layout__SBox-dxfASU.gxPtSz.Layout__ChartSection-cHvvpH.byJWks > div.Layout__SBox-dxfASU.fzJGzx.Layout__SFlex-dGOlJW.Xdeos.Layout__SCol-cKWydp.iDqTgD.Chart__Wrapper-idXQdb.jGsgR > div:nth-child(2) > div > div.Layout__SBox-dxfASU.hHtHrD.Layout__SFlex-dGOlJW.Xdeos.Layout__SCol-cKWydp.iDqTgD.ChartTooltip__Wrapper-gelFCO.ZhKbp.chart-tooltip > div > div.Layout__SBox-dxfASU.gezqkt.Layout__SFlex-dGOlJW.jzrRZA.Layout__SRow-jktCWH.hwJbaK > div:nth-child(2)')[0].text[:-1]
+    yesterday = soup.select('#__next > div.MainLayout__Wrapper-fslwZe.dhgRDa > div.MainLayout__Main-eibkLu.bNtKwG > div.Layout__SBox-dxfASU.gxPtSz.Layout__ChartSection-cHvvpH.byJWks > div.Layout__SBox-dxfASU.fzJGzx.Layout__SFlex-dGOlJW.Xdeos.Layout__SCol-cKWydp.iDqTgD.Chart__Wrapper-idXQdb.jGsgR > div:nth-child(2) > div > div.Layout__SBox-dxfASU.hHtHrD.Layout__SFlex-dGOlJW.Xdeos.Layout__SCol-cKWydp.iDqTgD.ChartTooltip__Wrapper-gelFCO.ZhKbp.chart-tooltip > div > div.Layout__SBox-dxfASU.gezqkt.Layout__SFlex-dGOlJW.jzrRZA.Layout__SRow-jktCWH.hwJbaK > div:nth-child(5)')[0].text[:-1]
+    today, yesterday = int(today.replace(',', '')), int(yesterday.replace(',', ''))
+    
+    return today, yesterday
+
+def cal_cases(n):
+    diff = n[0]-n[1]
+    if diff >= 0:
+        return f'전일 동시간대 대비 {diff}명 증가했습니다.'
+    else:
+        diff *= -1
+        return f'전일 동시간대 대비 {diff}명 감소했습니다.'
 
 client = commands.Bot(command_prefix='!')
-n = new_cases()
+n = crawl_cases()
+
+@tasks.loop(seconds=1)
+async def notice():
+    if datetime.datetime.now().minute == 0 and datetime.datetime.now().second == 0:
+        s = datetime.datetime.now().strftime('%Y-%m-%d %H')
+        await client.get_channel('(int)Your channel ID').send(f'{s}시 기준 확진자수는 {n[0]}명입니다.')
+        await client.get_channel('(int)Your channel ID').send(cal_cases(n))
+
+@client.event
+async def on_ready():
+    notice.start()
 
 @client.command()
 async def cc(ctx): #confirmed_cases
-    s = "신규 확진자 수: "+str(n)+"명"
-    await ctx.send(s)
+    s = datetime.datetime.now().strftime('%Y-%m-%d %H')
+    await ctx.send(f'{s}시 기준 확진자수는 {n[0]}명입니다.')
+    await ctx.send(cal_cases(n))
 
-client.run('ODczNjA3MDkwNzU5OTUwMzQ4.YQ64AQ.gzG7wpbzwhH172jrq-zpwgR3lFM')
+client.run('Your Token')
