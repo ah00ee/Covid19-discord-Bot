74924b7e5a9e6d4627fb0cd741a7de2bb366b2e3
ah00ee <nhoahyoung@gmail.com>
2021-08-14 21:36:04 +0900

    update

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from discord.ext import commands, tasks
 from pytz import timezone
 import datetime
-import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import os
+import asyncio
 
 def crawl_cases():
     options = webdriver.ChromeOptions()
@@ -20,7 +20,6 @@ def crawl_cases():
 
     url = 'https://corona-live.com/'
     driver.get(url)
-    time.sleep(1)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     today = soup.select('#__next > div.MainLayout__Wrapper-fslwZe.dhgRDa > div.MainLayout__Main-eibkLu.bNtKwG > div.Layout__SBox-dxfASU.gxPtSz.Layout__ChartSection-cHvvpH.byJWks > div.Layout__SBox-dxfASU.fzJGzx.Layout__SFlex-dGOlJW.Xdeos.Layout__SCol-cKWydp.iDqTgD.Chart__Wrapper-idXQdb.jGsgR > div:nth-child(2) > div > div.Layout__SBox-dxfASU.hHtHrD.Layout__SFlex-dGOlJW.Xdeos.Layout__SCol-cKWydp.iDqTgD.ChartTooltip__Wrapper-gelFCO.ZhKbp.chart-tooltip > div > div.Layout__SBox-dxfASU.gezqkt.Layout__SFlex-dGOlJW.jzrRZA.Layout__SRow-jktCWH.hwJbaK > div:nth-child(2)')[0].text[:-1]
     yesterday = soup.select('#__next > div.MainLayout__Wrapper-fslwZe.dhgRDa > div.MainLayout__Main-eibkLu.bNtKwG > div.Layout__SBox-dxfASU.gxPtSz.Layout__ChartSection-cHvvpH.byJWks > div.Layout__SBox-dxfASU.fzJGzx.Layout__SFlex-dGOlJW.Xdeos.Layout__SCol-cKWydp.iDqTgD.Chart__Wrapper-idXQdb.jGsgR > div:nth-child(2) > div > div.Layout__SBox-dxfASU.hHtHrD.Layout__SFlex-dGOlJW.Xdeos.Layout__SCol-cKWydp.iDqTgD.ChartTooltip__Wrapper-gelFCO.ZhKbp.chart-tooltip > div > div.Layout__SBox-dxfASU.gezqkt.Layout__SFlex-dGOlJW.jzrRZA.Layout__SRow-jktCWH.hwJbaK > div:nth-child(5)')[0].text[:-1]
@@ -36,12 +35,17 @@ def cal_cases(n):
         diff *= -1
         return f'전일 동시간대 대비 {diff}명 감소했습니다.'
 
+def now():
+    return datetime.datetime.now(timezone('Asia/Seoul'))
+
 client = commands.Bot(command_prefix='!')
-n = crawl_cases()
 
 @tasks.loop(seconds=1)
 async def notice():
-    if datetime.datetime.now(timezone('Asia/Seoul')).minute == 0 and datetime.datetime.now(timezone('Asia/Seoul')).second == 0:
+    if now().hour == 11:
+        await asyncio.sleep(35999)
+    if now().minute == 0 and now().second == 0:
+        n = crawl_cases()
         s = datetime.datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H')
         await client.get_channel(873623906907467887).send(f'{s}시 기준 확진자수는 {n[0]}명입니다.')
         await client.get_channel(873623906907467887).send(cal_cases(n))
@@ -52,6 +56,7 @@ async def on_ready():
 
 @client.command()
 async def cc(ctx): #confirmed_cases
+    n = crawl_cases()
     s = datetime.datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H')
     await ctx.send(f'{s}시 기준 확진자수는 {n[0]}명입니다.')
     await ctx.send(cal_cases(n))
